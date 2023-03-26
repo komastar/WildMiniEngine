4bb36bc686d8af97b30f1f7081d8fb49eea926c0
Eugene Kim <komastar.dev@gmail.com>
2023-03-26 15:35:16 +0900

    min max 추가

--- Code/WMFramework/WMFramework.h
+++ Code/WMFramework/WMFramework.h
@@ -55,4 +55,16 @@ namespace WildMini
     {
         return value < min ? min : value > max ? max : value;
     }
+
+    template<class T, class Comp = std::greater<void>, class Proj = std::identity>
+    const T& Max(const T& obj1, const T& obj2, Comp comp = {}, Proj proj = {})
+    {
+        return std::invoke(comp, std::invoke(proj, obj1), std::invoke(proj, obj2)) ? obj1 : obj2;
+    }
+
+    template<class T, class Comp = std::less<void>, class Proj = std::identity>
+    const T& Min(const T& obj1, const T& obj2, Comp comp = {}, Proj proj = {})
+    {
+        return std::invoke(comp, std::invoke(proj, obj1), std::invoke(proj, obj2)) ? obj1 : obj2;
+    }
 }
