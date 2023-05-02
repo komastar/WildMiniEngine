328097cabd71f20518feede207d316625cf4a69e
Eugene Kim <komastar.dev@gmail.com>
2023-05-02 22:31:51 +0900

    editor 비우기

--- Code/Editor/EditorApplication.cpp
+++ Code/Editor/EditorApplication.cpp
@@ -18,27 +18,7 @@
 
 using namespace WildMini;
 
-struct Constants
-{
-    WMMatrix4 viewProj;
-    WMMatrix4 world[3];
-};
-
-struct MainPassConstants
-{
-    WMVector3 eye;
-    WMVector3 light;
-};
-
-struct ProgressConstants
-{
-    WMMatrix4 viewProj;
-    WMVector4 time;
-};
-
 EditorApplication::EditorApplication()
-    : uiMesh(nullptr)
-    , needResize(false)
 {
 }
 
@@ -48,89 +28,14 @@ EditorApplication::~EditorApplication()
 
 void EditorApplication::OnInitialize()
 {
-    window = WindowFactory::Create(1280, 720);
-    window->Create();
-    window->Show();
-    window->Focus();
-    window->AddResizeCallback(std::bind(&EditorApplication::OnResize, this, std::placeholders::_1, std::placeholders::_2));
-    window->AddKeyboardEventHandler([&](WMKeyboardEvent keyboardEvent)
-        {
-            if (WMKey::RETURN == keyboardEvent.keycode)
-            {
-                CreateRenderPipeline();
-            }
-        });
-
-    device = GraphicsDeviceFactory::Create();
-    commandQueue = device->CreateCommandQueue();
-    swapChain = commandQueue->CreateSwapChain(window);
-
-    CreateRenderPipeline();
-
-    uiCamera.SetView(WMVector3{ 400.0f, 200.0f, -400.0f }, WMVector3::zero, WMVector3::up);
-    uiCamera.SetPerspective(0.25f, window->GetAspect(), 1.0f, 1000.0f);
-
-    float deltaTime = 0.0f;
-    gameThread = WMThread::Create(L"Editor");
-    gameThread->Initialize([&]()
-        {
-            auto begin = std::chrono::high_resolution_clock::now();
-            Update(deltaTime);
-            Render();
-            auto end = std::chrono::high_resolution_clock::now();
-            auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-            deltaTime = dt * 0.001f;
-        });
-    gameThread->Run();
 }
 
 void EditorApplication::CreateRenderPipeline()
 {
-    vertexShader = device->CreateShader(L"Resources/Shader/UIShader.hlsl", "VS", WMShader::StageType::Vertex);
-    pixelShader = device->CreateShader(L"Resources/Shader/UIShader.hlsl", "PS", WMShader::StageType::Fragment);
-
-    WMRenderPipelineDescriptor pipelineDesc = {};
-    pipelineDesc.sampleCount = 1;
-    pipelineDesc.vertexShader = vertexShader;
-    pipelineDesc.fragmentShader = pixelShader;
-    pipelineDesc.vertexDescriptor.attributes = {
-        { WMVertexFormat::Float3, "POSITION",       0, 0  },
-        { WMVertexFormat::Float2, "TEXCOORD",       0, 12 },
-        { WMVertexFormat::Float4, "COLOR",          0, 20 },
-        { WMVertexFormat::Uint,   "SV_InstanceID",  0, 28 },
-    };
-
-    WMRenderPipelineColorAttachmentDescriptor colorAttach;
-    pipelineDesc.colorAttachments = { colorAttach };
-
-    pipelineDesc.depthStencilPixelFormat = WMPixelFormat::DEPTH_24_UNORM_STENCIL_8;
-    pipelineDesc.inputPrimitiveTopology = WMPrimitiveTopologyType::Triangle;
-
-    renderPipeline = device->CreateRenderPipeline(pipelineDesc);
 }
 
 void EditorApplication::OnTerminate()
 {
-    if (gameThread)
-    {
-        gameThread->Terminate();
-    }
-
-    if (uiMesh)
-    {
-        delete uiMesh;
-        uiMesh = nullptr;
-    }
-
-    progressBuffer = nullptr;
-    commandQueue = nullptr;
-    swapChain = nullptr;
-    vertexShader = nullptr;
-    pixelShader = nullptr;
-    renderPipeline = nullptr;
-    gameThread = nullptr;
-    device = nullptr;
-    window = nullptr;
 }
 
 void EditorApplication::Update(float dt)
@@ -139,37 +44,4 @@ void EditorApplication::Update(float dt)
 
 void EditorApplication::Render()
 {
-    if (needResize)
-    {
-        needResize = false;
-        swapChain->Resize(window->width, window->height);
-        uiCamera.SetPerspective(0.25f, window->GetAspect(), 1.0f, 1000.0f);
-    }
-
-    if (WMSharedPtr<WMCommandBuffer> commandBuffer = commandQueue->CreateCommandBuffer())
-    {
-        if (WMSharedPtr<WMRenderCommandEncoder> renderCommandEncoder = commandBuffer->CreateRenderCommandEncoder(renderPipeline))
-        {
-            renderCommandEncoder->ClearRenderTarget(swapChain->RenderTargetTexture(), WMColor::black);
-            renderCommandEncoder->ClearDepthStencil(swapChain->DepthStencilTexture()
-                , WMRenderCommandEncoder::DepthStencilClearFlag::All
-                , 0.0f
-                , 0);
-            WMViewport viewport = { 0, 0, window->GetWidth(), window->GetHeight(), 0.0f, 1.0f };
-            renderCommandEncoder->SetViewport(viewport);
-            WMRect scissorRect = { 0, 0, window->GetWidth(), window->GetHeight() };
-            renderCommandEncoder->SetScissorRect(scissorRect);
-            renderCommandEncoder->SetRenderTargets({ swapChain->RenderTargetTexture() }, swapChain->DepthStencilTexture());
-            renderCommandEncoder->EndEncoding({ swapChain->RenderTargetTexture() });
-        }
-        commandBuffer->Commit();
-    }
-
-    swapChain->Present();
-    commandQueue->WaitComplete();
-}
-
-void EditorApplication::OnResize(uint32_t width, uint32_t height)
-{
-    needResize.store(true);
 }
--- Code/Editor/EditorApplication.h
+++ Code/Editor/EditorApplication.h
@@ -10,15 +10,6 @@
 #include "WMSharedPtr.h"
 #include "Application/WMApplication.h"
 #include "Window/WMWindow.h"
-#include "Graphics/WMGraphicsDevice.h"
-#include "Graphics/WMCommandQueue.h"
-#include "Graphics/WMSwapChain.h"
-#include "Graphics/WMGPUBuffer.h"
-#include "Graphics/WMShader.h"
-#include "Graphics/WMRenderPipeline.h"
-#include "Graphics/WMCamera.h"
-#include "Graphics/Geometry/WMMesh.h"
-#include "Common/WMThread.h"
 
 namespace WildMini
 {
@@ -37,24 +28,7 @@ namespace WildMini
         void Update(float dt);
         void Render();
 
-        void OnResize(uint32_t width, uint32_t height);
-
     private:
         WMSharedPtr<WMWindow> window;
-        WMSharedPtr<WMGraphicsDevice> device;
-
-        WMSharedPtr<WMThread> gameThread;
-        WMSharedPtr<WMCommandQueue> commandQueue;
-        WMSharedPtr<WMSwapChain> swapChain;
-
-        WMSharedPtr<WMShader> vertexShader;
-        WMSharedPtr<WMShader> pixelShader;
-        WMSharedPtr<WMRenderPipeline> renderPipeline;
-
-        WMSharedPtr<WMGPUBuffer> progressBuffer;
-        WMCamera uiCamera;
-        WMMesh* uiMesh;
-
-        std::atomic_bool needResize;
     };
 }
